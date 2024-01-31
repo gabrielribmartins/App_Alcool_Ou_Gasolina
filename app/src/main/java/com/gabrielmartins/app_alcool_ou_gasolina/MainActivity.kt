77b5db956355dd41d58da6aa26e06e7dc57eb29c
Gabriel Martins <gabrielribmartins@gmail.comn>
2024-01-31 09:46:36 -0300

    Final Commit

---
+++ app/src/main/java/com/gabrielmartins/app_alcool_ou_gasolina/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gabrielmartins.app_alcool_ou_gasolina
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
